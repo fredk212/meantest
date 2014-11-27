ae6e9c936ed7e2f96d9a02cf88265aeab2be88ed
fredk212 <ffkhoury@crimson.ua.edu>
2014-11-27 09:20:10 -0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
 	// the database url to connect
-	url : 'mongodb://fsandboxuser:fsandboxpass@ds053140.mongolab.com:53140/fredsandbox'
+	url : 'mongodb://USERNAME:PASSWORD@ds053140.mongolab.com:53140/fredsandbox'
 }
